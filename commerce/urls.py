fe0da2ed8b246864214b79c66c2aeefbf5b03d6f
Adeyemi Oluwatobi Sunday <47352532+diddyjax19@users.noreply.github.com>
2023-02-27 13:59:44 +0100

    Add files via upload

---
+++ commerce/urls.py
@@ -0,0 +1,11 @@
+
+
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("auctions.urls"))
+]
+
+handler404 = "auctions.views.handle_not_found"
